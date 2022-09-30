5f0ef92bcce18f4c78386485679c743e1683bb3d
EduardoLopezz <113383821+EduardoLopezz@users.noreply.github.com>
2022-09-30 12:43:33 -0600

    Add files via upload

---
+++ Eduardo_Pagi/index.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="styles.css">
+    <link rel="stylesheet" href="reset.css">
+</head>
+<body>
+ <header class="main-hader">
+        <nav class="main-nav">
+            <div class="logo">
+                <span>MAZDA</span>
+            </div>
+            <ul>
+                <li><a href="#">Estilos</a></li>
+                <li><a href="#">Diseños</a></li>
+                <li><a href="#">Modelos</a></li>
+                <li><a href="#">Versiones</a></li>
+            </ul>
+        </nav>
+        <div class="content-header">
+           <h1>Vehiculos/Motocicletas</h1> 
+           <p>Todo tipo de transporte favorito</p>
+            <a href="#">Ver informacion</a>
+        </div>
+ </header>
+</body>
+</html>
